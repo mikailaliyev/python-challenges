864086f18f4937631d2a430a4876e7542e663f53
Mikayil <mikail.aliyev@gmail.com>
2023-04-03 09:48:23 +0400

    working with integers&conversion
    Converting string input into integers to make possible to do some math with them

---
+++ 004.py
@@ -0,0 +1,4 @@
+# Ask the user to enter two numbers. Add them together and display the answer as The total is [answer]
+first_number = int(input("enter the first number:\n"))
+second_number = int(input("enter the second number:\n"))
+print(f"The total is {first_number + second_number}")
