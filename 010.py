e2b0e935925f3f02d080b1fa0f0096d5aeef7760
Mikayil <mikail.aliyev@gmail.com>
2023-04-03 11:49:23 +0400

    converting kilograms to pounds

---
+++ 010.py
@@ -0,0 +1,6 @@
+# There are 2,204 pounds in a kilogram. 
+# Ask the user to enter a weight in kilograms and convert it to pounds.
+
+kilograms = float(input("Enter kilograms\n"))
+pounds = kilograms *  2.204
+print(f"{kilograms} kilograms equal to {round(pounds, 2)} pounds")
