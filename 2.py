5f53bf929ec3107cfb55b9928293226cc65c1807
Mikayil <mikail.aliyev@gmail.com>
2023-04-03 09:41:07 +0400

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,3 @@
+first_name = input("What is your first name?\n").lower().capitalize()
+surname_name = input("What is your surname name?\n").lower().capitalize()
+print(f"Hello {first_name} {surname_name}!")
