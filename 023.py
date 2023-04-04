cf903f94c4a65448630c08175288c9c6f77086d7
Mikayil <mikail.aliyev@gmail.com>
2023-04-04 15:54:42 +0400

    Update 023.py

--- 023.py
+++ 023.py
@@ -2,6 +2,7 @@
 # Ask for a starting number and an ending number and then display just that section of the text 
 
 nursery_rhyme = input("Enter the first line of a nursery rhyme\n")
+print(len(nursery_rhyme)
 starting_number = int(input("Enter a starting number\n"))
 ending_number = int(input("Enter an ending number\n"))
 print(nursery_rhyme[starting_number: ending_number])
