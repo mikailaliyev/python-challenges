2cf524b5a6d3d7fc01cb980d66699d1a0a0f7156
Mikayil <mikail.aliyev@gmail.com>
2023-04-04 11:18:15 +0400

    using len() function

---
+++ 020.py
@@ -0,0 +1,4 @@
+# Ask the user to enter their first name and then display the length of their name.
+
+user_first_name = input("What is your first name?\n")
+print(f"Your name's length is: {len(user_first_name)}")
