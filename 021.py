0bef9d463cd73edc11b559d25848651cbfb99189
Mikayil <mikail.aliyev@gmail.com>
2023-04-04 11:20:43 +0400

    using concatenation

---
+++ 021.py
@@ -0,0 +1,9 @@
+# Ask the user to enter their first name 
+# and then ask them to enter their surname. 
+# Join them together with a space between
+# and display the name and the length of whole name.
+
+first_name = input("What is your first name?\n")
+last_name = input("What is your last name?\n")
+full_name = first_name + " " + last_name
+print(f"Your full name is: {full_name}")
