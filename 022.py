3fdd665bc1502c351cf2be25a3a38565e9b19858
Mikayil <mikail.aliyev@gmail.com>
2023-04-04 11:24:18 +0400

    using title() method

---
+++ 022.py
@@ -0,0 +1,7 @@
+# Ask the user to enter their first name and surname in lower case. 
+# Change the case to title case and join them together.
+# Display the finished result.
+
+full_name = input("Enter your full name in lower case\n")
+capitalize = full_name.title()
+print(capitalize)
