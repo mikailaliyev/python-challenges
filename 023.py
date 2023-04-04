18c33a46127674e230a024a8eefcee8036efe79f
Mikayil <mikail.aliyev@gmail.com>
2023-04-04 15:34:59 +0400

    slicing strings

---
+++ 023.py
@@ -0,0 +1,7 @@
+# Ask the user to type in the first line of a nursery rhyme and display the length of the string.
+# Ask for a starting number and an ending number and then display just that section of the text 
+
+nursery_rhyme = input("Enter the first line of a nursery rhyme\n")
+starting_number = int(input("Enter a starting number\n"))
+ending_number = int(input("Enter an ending number\n"))
+print(nursery_rhyme[starting_number: ending_number])
