f6c010b4964353bb6f39c71a62bffa0b90ff1ede
Mikayil <mikail.aliyev@gmail.com>
2023-04-05 09:33:23 +0400

    floating point numbers

---
+++ 027.py
@@ -0,0 +1,6 @@
+# Ask the user to enter a number with lots of decimal places. 
+# Multiply this number by two and display the answer.
+
+user_number = float(input("Enter a float point number with lots of decimal places\n"))
+multiplied = user_number * 2
+print(multiplied)
