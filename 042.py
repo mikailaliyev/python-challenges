ec73eceb7a383d9d29d7ed2b275f3bde6678922b
Mikayil <mikail.aliyev@gmail.com>
2023-04-06 09:52:42 +0400

    looping conditionally with the result showed

---
+++ 042.py
@@ -0,0 +1,14 @@
+# Set a variable called total to 0. 
+# Ask the user to enter five numbers and after each input ask them if they want that number included. 
+# If they do, then add the number to the total. 
+# If they do not want it included, don’t add it to the total. 
+# After they have entered all five numbers, display the total
+
+total = 0
+for i in range(0, 5):
+    user_number = int(input("Enter a number\n"))
+    user_choice = input("Do you want that number included?\nyes/no\n")
+    if user_choice == "yes":
+        total += user_number
+
+print(total)
