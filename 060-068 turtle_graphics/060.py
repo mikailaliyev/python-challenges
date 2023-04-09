4afdf8e82747bbe6698b3d12165e8ca27d044c69
mikailaliyev <mikail.aliyev@gmail.com>
2023-04-09 21:54:10 +0400

    turtle basics

---
+++ 060-068 turtle_graphics/060.py
@@ -0,0 +1,10 @@
+# Draw a square
+import turtle
+
+turtle.Screen().bgcolor("black")
+turtle.color("white", "white")
+for i in range(0, 4):
+    turtle.forward(100)
+    turtle.right(90)
+
+turtle.exitonclick()
